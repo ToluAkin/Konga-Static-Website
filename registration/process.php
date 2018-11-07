f3a7ed933b0a321d809fd74286c74ae9da00042d
ToluAkin <38213766+ToluAkin@users.noreply.github.com>
2018-11-07 12:40:46 +0100

    Removed function in success.html & replaced { with ;

--- registration/process.php
+++ registration/process.php
@@ -6,33 +6,40 @@
         $confirm_password = $_POST['confirm_passsword'];
         $phone_number = $_POST['phone_number'];
         $gender = $_POST['gender'];
-        $country = $_POST['country'];
-        saveToFile($first_name, $last_name, $email_address, $password, $confirm_password, $phone_number, $gender, $country); 
+        $country = $_POST['country'];  
+        saveToFile($first_name, $last_name, $email_address, $password, $confirm_password, $phone_number, $gender, $country);
         header('Location:success.html');
+
     function saveToFile($first_name, $last_name, $email_address, $password, $confirm_password, $phone_number, $gender, $country) {   
         $fileHandler = fopen('record.txt', 'a');   
         $string = $first_name . ',' . $last_name . ',' . $email_address . ',' . $password . ',' . $confirm_password . ',' . $phone_number . ',' . $gender . ',' . $country . "\n";   
         fwrite($fileHandler, $string);   
         fclose($fileHandler); 
-}
-    function saveToDatabase($name, $email) {   
+    }
+
+    function saveToDatabase($first_name, $last_name, $email_address, $password, $confirm_password, $phone_number, $gender, $country) {   
         $serverName = "localhost";   
         $database = "Registration";   
         $username = "root";   
         $password = "";
+
     //Open database connection   
     $conn = mysqli_connect($serverName, $username, $password, $database);
+
     // Check that connection exists   
     if (!$conn) {       
         die("Connection failed: " . mysqli_connect_error());  
      }   
-       $sql = "INSERT INTO users (first_name, last_name, email_address, password, confirm_password, phone_number, gender, country, created_at)       VALUES ('$first_name', 'last_name', '$email_address', 'password', 'confirm_password', 'phone_number', 'gender', 'country', NOW())";  
+
+       $sql = "INSERT INTO users (first_name, last_name, email_address, password, confirm_password, phone_number, gender, country, created_at)  VALUES ('$first_name', 'last_name', '$email_address', 'password', 'confirm_password', 'phone_number', 'gender', 'country', NOW())";  
         $result = mysqli_query($conn, $sql);
+
     //Check for errors   
     if (!$result) {     
           die("Error: " . $sql . "<br>" . mysqli_error($conn));  
-         }  
+        }  
+
     //Close the connection   
     mysqli_close($conn); 
-}
+    }
 ?>
